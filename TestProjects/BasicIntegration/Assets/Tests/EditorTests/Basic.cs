39625e7d1b5b0994aef8fbcdbb48d5acb25bef62
Tomas Dirvanauskas <tomas@unity3d.com>
2024-03-08 17:32:02 +0200

    Add test project

---
+++ TestProjects/BasicIntegration/Assets/Tests/EditorTests/Basic.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
