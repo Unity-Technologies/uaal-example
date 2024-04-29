90ccdaba60c71af1a175b865addbe021da15c694
Tomas Dirvanauskas <tomas@unity3d.com>
2024-04-29 11:40:29 +0300

    temporary error, to see if merge will be blocked

--- UnityProject/Assets/Plugins/Android/MainApp.androidlib/src/main/java/com/unity/mynativeapp/MainActivity.java
+++ UnityProject/Assets/Plugins/Android/MainApp.androidlib/src/main/java/com/unity/mynativeapp/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+sdsds
 
 public class MainActivity extends AppCompatActivity {
     private enum ActivityType {
